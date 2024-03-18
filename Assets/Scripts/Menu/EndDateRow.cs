17fd8a0782c2299da6fac52b150e95da4d53e96f
Chloe Law <chloelaw75@hotmail.com>
2024-03-18 19:37:44 -0400

    Update the aspect ratio to the screen size 800x480
    Modify EndDateRows.cs so the "Expected End Date" would always show

--- Assets/Scripts/Menu/EndDateRow.cs
+++ Assets/Scripts/Menu/EndDateRow.cs
@@ -7,16 +7,21 @@ public class EndDateRow : MonoBehaviour
 {
     public TimeRow timeRow;
     TMPro.TMP_Text text;
+    public GameObject date;
 
     private void Start()
     {
-        text = GetComponentInChildren<TMPro.TMP_Text>();
+        text = date.GetComponent<TMPro.TMP_Text>();
+        //text = GetComponentInChildren<TMPro.TMP_Text>();
         timeRow.OnUpdateDesiredEndDate += UpdateTimeRow;
         text.text = "";
     }
 
     public void UpdateTimeRow(object sender, EventArgs e)
     {
-        text.text = "Expected end date: " + timeRow.DesiredDateEnd.ToString();
+        //text.text = "Expected end date: " + timeRow.DesiredDateEnd.ToString();
+        text.text = " " + timeRow.DesiredDateEnd.ToString();
     }
 }
+
+// I edit so the "Expected End Date" would always show
